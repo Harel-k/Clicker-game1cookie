ec19a69be19b0396da03bf686387622c1f55680d
Harel_k <harelikatz@gmail.com>
2025-02-05 17:29:37 +0200

    .

---
+++ game.js
@@ -0,0 +1,34 @@
+let total = document.getElementById('total');
+
+let amount = 0;
+
+function point(){
+    amount = amount+1;
+
+    total.innerHTML = amount;
+
+    let clickAudio = new Audio("click.wav");
+    clickAudio.play();
+
+    console.log("Amount is:", amount, "Type:", typeof amount);
+};
+
+function reset(){
+    alert('points have been reset!');
+
+    amount = 0;
+
+    total.innerHTML = amount;
+
+    let resetAudio = new Audio("reset.wav");
+    resetAudio.play();
+
+    console.log("Cookie Amount is:", amount, "Type:", typeof amount);
+};
+
+if(amount == 100){
+    console.log("The amount is 100!");
+
+    let winAudio = new Audio("win.mp3");
+    winAudio.play();
+}
